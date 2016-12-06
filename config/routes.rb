f13fd2c644a6d638ef4b2a4d8f64f438c5d19dde
shownola <swasieko@gmail.com>
2016-12-06 20:47:37 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
   
  root 'pages#home'
   get 'about', to: 'pages#about'
@@ -13,6 +11,13 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
+  resources :categories, except: [:destroy]
+  
+  
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
  
